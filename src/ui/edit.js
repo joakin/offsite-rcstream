9c2c847db09c42bb8412e8b242ab76037d6824fd
jdlrobson <jdlrobson@gmail.com>
2015-10-23 10:06:55 +0200

    Style edit summarys with logo
    * Add EditSummary class
    * Incorporate logo

--- src/ui/edit.js
+++ src/ui/edit.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Logo from './logo'
+import EditSummary from './edit-summary'
 
 export default React.createClass({
   render: function() {
     let {title, comment, wiki, bot} = this.props.edit
     return (
       <div className='Edit'>
-        <h3>{title}</h3>
-        <p>{wiki}: {comment}</p>
+        <Logo wiki={wiki}></Logo>
+        <EditSummary title={title} comment={comment}></EditSummary>
       </div>
     )
   }
